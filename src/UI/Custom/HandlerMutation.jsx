92ace25b972141aa2d15ca8a6d7b7a7ac9c049c5
viyaya <johndoe@email.com>
2024-11-02 17:39:54 +0300

    WaveCss

--- src/UI/Custom/HandlerMutation.jsx
+++ src/UI/Custom/HandlerMutation.jsx
@@ -3,8 +3,13 @@ import classes from "./HandlerMutation.module.css";
 import icon from "../image/iconHeader.svg";
 import error from "../image/error.svg";
 import success from "../image/success.svg";
-export default function HandlerMutation({Loading, Error, Success, textSuccess, textError}) {
-
+export default function HandlerMutation({
+  Loading,
+  Error,
+  Success,
+  textSuccess,
+  textError,
+}) {
   const [showSuccessMutation, setShowSuccessMutation] = useState(false);
   const [showErrorMutation, setShowErrorMutation] = useState(false);
 
@@ -42,8 +47,19 @@ export default function HandlerMutation({Loading, Error, Success, textSuccess, t
       {Loading && (
         <div className={classes.load}>
           <img src={icon} alt="Loading..." className={classes.loadImage} />
-          <div>
-            <span className={classes.spanLoad}>Идет загрузка...</span>
+          <div className={classes.wave}>
+            {/* <span className={classes.spanLoad}>Загрузка...</span> */}
+            <span style={{ "--i": 1 }}>З</span>
+            <span style={{ "--i": 2 }}>А</span>
+            <span style={{ "--i": 3 }}>Г</span>
+            <span style={{ "--i": 4 }}>Р</span>
+            <span style={{ "--i": 5 }}>У</span>
+            <span style={{ "--i": 6 }}>З</span>
+            <span style={{ "--i": 7 }}>К</span>
+            <span style={{ "--i": 8 }}>А</span>
+            <span style={{ "--i": 9 }}>.</span>
+            <span style={{ "--i": 10 }}>.</span>
+            <span style={{ "--i": 11 }}>.</span>
           </div>
         </div>
       )}
@@ -51,9 +67,7 @@ export default function HandlerMutation({Loading, Error, Success, textSuccess, t
       {showErrorMutation && (
         <div className={classes.error}>
           <img src={error} alt="Error" className={classes.errorImage} />
-          <span className={classes.spanError}>
-            {textError}
-          </span>
+          <span className={classes.spanError}>{textError}</span>
         </div>
       )}
 
--- src/UI/Custom/HandlerQeury.jsx
+++ src/UI/Custom/HandlerQeury.jsx
@@ -3,14 +3,25 @@ import classes from "./HandlerQeury.module.css";
 import icon from "../image/iconHeader.svg";
 import error from "../image/error.svg";
 
-export default function HandlerQeury({Loading, Fetching, Error}) {
+export default function HandlerQeury({ Loading, Fetching, Error }) {
   return (
     <>
       {(Fetching || Loading) && (
         <div className={classes.load}>
           <img src={icon} alt="Loading..." className={classes.loadImage} />
-          <div>
-            <span className={classes.spanLoad}>Идет загрузка...</span>
+          <div className={classes.wave}>
+            {/* <span className={classes.spanLoad}>Загрузка...</span> */}
+            <span style={{ "--i": 1 }}>З</span>
+            <span style={{ "--i": 2 }}>А</span>
+            <span style={{ "--i": 3 }}>Г</span>
+            <span style={{ "--i": 4 }}>Р</span>
+            <span style={{ "--i": 5 }}>У</span>
+            <span style={{ "--i": 6 }}>З</span>
+            <span style={{ "--i": 7 }}>К</span>
+            <span style={{ "--i": 8 }}>А</span>
+            <span style={{ "--i": 9 }}>.</span>
+            <span style={{ "--i": 10 }}>.</span>
+            <span style={{ "--i": 11 }}>.</span>
           </div>
         </div>
       )}
@@ -18,9 +29,7 @@ export default function HandlerQeury({Loading, Fetching, Error}) {
       {Error && (
         <div className={classes.error}>
           <img src={error} alt="Error" className={classes.errorImage} />
-          <span className={classes.spanError}>
-            Ошибка
-          </span>
+          <span className={classes.spanError}>Ошибка</span>
         </div>
       )}
     </>
